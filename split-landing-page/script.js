a7e2eb56df7d8b9b5d7f2521c5588b7683534607
Purvesh <purveshpaunikar@gmail.com>
2022-09-04 00:42:41 +0530

    split-landing page project

---
+++ split-landing-page/script.js
@@ -0,0 +1,33 @@
+const containers = document.querySelector('.container')
+const left    = document.querySelector('.left')
+const right   = document.querySelector('.right')
+
+
+/* we can use mouseover = mouseout or mouseenter + mouseleave */
+
+// This is my method 
+
+left.addEventListener('mouseover', () =>{
+    containers.classList.toggle('hover-left')
+})
+left.addEventListener('mouseout', () =>{
+    containers.classList.toggle('hover-left')
+})
+
+right.addEventListener('mouseover', () =>{
+    containers.classList.toggle('hover-right')
+})
+right.addEventListener('mouseout', () =>{
+    containers.classList.toggle('hover-right')
+})
+
+// this is Brad method 
+
+/*
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+
+*/
